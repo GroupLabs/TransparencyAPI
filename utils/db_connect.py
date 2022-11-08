2d000df138c1597f10cecbc95ba093031386064f
noelfranthomas <noelfranthomas@gmail.com>
2022-11-07 20:50:35 -0700

    added streamlit interface

---
+++ utils/db_connect.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+import pymongo
+
+load_dotenv()
+
+def connectMongo():
+    # Connection
+    conn_str = f"mongodb+srv://admin:{os.getenv('MONGO_PASS')}@cluster0.dm2jouu.mongodb.net/?retryWrites=true&w=majority"
+
+    client = pymongo.MongoClient(conn_str)
+
+    return client
