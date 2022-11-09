1373c98ae033c0231abb9c1bb6abecba3788d61b
noelfranthomas <noelfranthomas@gmail.com>
2022-11-08 21:36:22 -0700

    Update db_connect.py

--- utils/db_connect.py
+++ utils/db_connect.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 def connectMongo():
     # Connection
-    conn_str = f"mongodb+srv://admin:{os.getenv('MONGO_PASS')}@cluster0.dm2jouu.mongodb.net/?retryWrites=true&w=majority"
+    conn_str = f"mongodb+srv://{os.getenv('MONGO_USER')}:{os.getenv('MONGO_PASS')}@cluster0.dm2jouu.mongodb.net/?retryWrites=true&w=majority"
 
     client = pymongo.MongoClient(conn_str)
 
