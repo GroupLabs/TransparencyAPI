5537a85cb3b06aebace83f1b68bfaf40527596a1
noelfranthomas <noelfranthomas@gmail.com>
2022-12-22 13:28:33 -0700

    small update

--- st_interface.py
+++ st_interface.py
@@ -8,55 +8,68 @@ from src.st_utils.get_data import *
 
 st.title('Transparency API Interface')
 
-# Using object notation
-search_selectbox = st.sidebar.selectbox(
-    "How would you like to search?",
-    ("Meeting ID", "Date", "Meeting Type")
-)
+page = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Home', 'Search'))
 
-if search_selectbox == "Meeting ID":
-    meeting_id = st.text_input('Meeting ID:')
-    if meeting_id:
-        st.write('Selected ID: ', meeting_id)
-        data = get_meeting_by_id(meeting_id)
-        st.write(data)
+if page == 'Home':
+   print("Home")
 
-if search_selectbox == "Date":
-    time_radio = st.radio(
-        "Time included?",
-        ("Yes", "No")
+if page == 'Search':
+    # Using object notation
+    search_selectbox = st.selectbox(
+        "How would you like to search?",
+        ("Meeting ID", "Date Range", "Meeting Type")
     )
 
-    if time_radio == "Yes":
-        date = st.date_input('Date:').strftime("%A, %B %d, %Y")
-        time = st.time_input('Time:').strftime("%-I:%M %p")[:-1] + ".m."
+    if search_selectbox == "Meeting ID":
+        meeting_id = st.text_input('Meeting ID:')
+        if meeting_id:
+            st.write('Selected ID: ', meeting_id)
+            data = get_meeting_by_id(meeting_id)
+            st.write(data)
 
-        date_time = f"{date} at {time}"
+    if search_selectbox == "Date Range":
+        time_radio = st.radio(
+            "Include time?",
+            ("Yes", "No")
+        )
 
-        if date_time:
-            st.write('Selected Date: ', date_time)
-            data = get_meeting_by_datetime(date_time)
-            if data:
+        if time_radio == "Yes":
+            date = st.date_input('Date:').strftime("%A, %B %d, %Y")
+            time = st.time_input('Time:').strftime("%-I:%M %p")[:-1] + ".m."
+
+            date_time = f"{date} at {time}"
+
+            if date_time:
+                st.write('Selected Date: ', date_time)
+                data = get_meeting_by_datetime(date_time)
+                if data:
+                    num = st.number_input("Meeting Number", step=1, max_value=len(data)-1, min_value=0)
+                    st.download_button("download", json.dumps(data[num], indent=2).encode('utf-8'))
+                    st.write(data[num])
+
+        elif time_radio == "No":
+            date = st.date_input('Date:').strftime("%A, %B %d, %Y")
+            if date:
+                st.write('Selected Date: ', date)
+                data = get_meeting_by_date(date)
+                if data:
+                    num = st.number_input("Meeting Number", step=1, max_value=len(data)-1, min_value=0)
+                    st.download_button("download", json.dumps(data[num], indent=2).encode('utf-8'))
+                    st.write(data[num])
+
+    if search_selectbox == "Meeting Type":
+        meeting_type = st.text_input('Meeting Type:')
+        if meeting_type:
+            st.write('Selected Type: ', meeting_type)
+            data = get_meeting_by_type(meeting_type)   
+            if data:     
                 num = st.number_input("Meeting Number", step=1, max_value=len(data)-1, min_value=0)
-                # st.download_button("download", json.dumps(data[num], indent=2).encode('utf-8'))
-                st.write(data[num])
 
-    elif time_radio == "No":
-        date = st.date_input('Date:').strftime("%A, %B %d, %Y")
-        if date:
-            st.write('Selected Date: ', date)
-            data = get_meeting_by_date(date)
-            if data:
-                num = st.number_input("Meeting Number", step=1, max_value=len(data)-1, min_value=0)
-                # st.download_button("download", json.dumps(data[num], indent=2).encode('utf-8'))
-                st.write(data[num])
+                if st.button('Preview'):
+                    st.write(data[num])
+                # else:
 
-if search_selectbox == "Meeting Type":
-    meeting_type = st.text_input('Meeting Type:')
-    if meeting_type:
-        st.write('Selected Type: ', meeting_type)
-        data = get_meeting_by_type(meeting_type)   
-        if data:     
-            num = st.number_input("Meeting Number", step=1, max_value=len(data)-1, min_value=0)
-            # st.download_button("download", json.dumps(data[num], indent=2).encode('utf-8'))
-            st.write(data[num])
+                    # st.download_button("download", json.dumps(data[num], indent=2).encode('utf-8'))
+                
